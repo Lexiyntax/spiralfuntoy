9629c67d57c81582973ce06f85bad7a48e9b1559
Daniel E. Markle <dmarkle@ashtech.net>
2017-03-01 00:34:38 -0500

    Initial implementation using pop

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # spiralfuntoy
 A spiral parsing toy program in Python
+
+Take an array separated by spaces like:
+```
+1 2 3
+4 5 6
+7 8 9
+```
+
+And flatten it as if it was being spirally peeled like a potato into:
+```
+1 2 3 6 9 8 7 4 5
+```
+
+Or to think of it differently:
+```
+1 2 3
+8 9 4
+7 6 5
+```
+
+Output as:
+```
+1 2 3 4 5 6 7 8 9
+```
+
+Being careful to not be dependent on actual contents.
+
---
+++ array.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+spiral = []
+with open('array.txt','r') as a:
+    for line in a:
+        row = map(lambda x: x.rstrip(), line.split(' '))
+        spiral.append(row)
+
+print str(len(spiral)) + " by " + str(len(spiral[0]))
+
+# note this implementation is destructive to the original; if that needs
+#   preserved, copy it first
+reformed = []
+while (len(spiral) > 0):
+    # chuck out the top of the list (easy part)
+    reformed.append( spiral.pop(0) )
+    # go down the right side
+    for x in spiral:
+        reformed.append( x.pop() )
+    # pop off the bottom (reversing does in-place so we have to handle that)
+    if (len(spiral) > 0):
+        l = spiral.pop()
+        l.reverse()
+        reformed.append( l )
+    # go up the left side
+    if (len(spiral) > 0):
+        for x in reversed(spiral):
+            reformed.append( x.pop(0) )
+
+print reformed
